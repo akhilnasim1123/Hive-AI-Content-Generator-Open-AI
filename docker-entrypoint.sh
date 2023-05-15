a9305fb4ae60b91496c05ffd4977ec25795bf7ad
akhil <akhilnasim029@gmail.com>
2023-05-15 13:47:07 +0530

    host

--- docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-# Perform environment variable substitution on nginx.conf.template
-envsubst '$$PORT' < /etc/nginx/nginx.conf> /etc/nginx/nginx.conf
-
-# Execute the CMD from the Dockerfile
-exec "$@"
