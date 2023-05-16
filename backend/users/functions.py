cf959a6060b216b7d0178fc465ce28d4a8fc5487
akhil <akhilnasim029@gmail.com>
2023-05-16 19:54:11 +0530

    host

--- backend/users/functions.py
+++ backend/users/functions.py
@@ -4,7 +4,7 @@ import openai
 from backend import settings
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.environ.get('OPENAI_API_KEY')
-
+print(os.environ.get('DATABASE_PASSWORD'))
 
 def generateBlogTopicIdeas(topic, keywords):
     blog_topics = []
