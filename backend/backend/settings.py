c447508672a6981376fd21268c94ddf70f19fb10
akhil <akhilnasim029@gmail.com>
2023-05-16 15:57:57 +0530

    host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,9 +167,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER =os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER ='akhilnasim1123@gmail.com'
 print(os.environ.get('EMAIL_HOST_PASSWORD'),'sadfasdddddddddddddddddddddddddddd')
-EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_PASSWORD = 'xumqgyhgihpvulea'
 
 
 OPENAI_API_KEYS=os.environ.get('OPENAI_API_KEY')
